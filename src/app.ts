410e00777f27e89bb2b9d856bc615d913b593d15
macosta08 <mariangelacosta.88@gmail.com>
2025-01-20 21:48:30 -0500

    Configuración inicial de Node con TypeScript con Nodemon

--- src/app.ts
+++ src/app.ts
@@ -2,4 +2,4 @@ import { findHeroById } from "./services/hero.service";
 
 const hero = findHeroById(2);
 
-console.log(hero?.name ?? "No hero found !!"); // hero = undefined.name e
+console.log(hero?.name ?? "No hero found !!"); // hero = undefined.name
